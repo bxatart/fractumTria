ab5215c4e62edf48c68a30076cfe2b4f980b3596
Berta Juanhuix Xatart <bjuanhuixxa@gmail.com>
2025-11-03 20:58:52 +0100

    Creació del projecte i estructura de carpetes

---
+++ scripts/GameState.gd
@@ -0,0 +1,14 @@
+extends Node
+
+## Color actual del jugador
+var current_color: StringName = "green"
+
+## Senyal per indicar el canvi de color
+signal color_changed(new_color: StringName)
+
+## Funció per canviar de color
+func set_color(new_color: StringName) -> void:
+	if current_color == new_color:
+		return
+	current_color = new_color
+	emit_signal("color_changed", current_color)
